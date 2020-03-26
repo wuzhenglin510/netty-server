e68cc9e16b7462581869defc5ffe798f0243876c
巫政霖 <wuzhenglin510@163.com>
2020-03-26 20:48:50 +0800

    init

---
+++ src/main/java/com/chat/handler/DispatcherHandler.java
@@ -0,0 +1,12 @@
+package com.chat.handler;
+
+import com.chat.proto.TopLevelDataOuterClass;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class DispatcherHandler extends SimpleChannelInboundHandler<TopLevelDataOuterClass.TopLevelData> {
+
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TopLevelDataOuterClass.TopLevelData topLevelData) throws Exception {
+        System.out.println("读取到数据: " + topLevelData.toString());
+    }
+}
--- src/main/java/com/chat/handler/IdleDestroyHandler.java
+++ src/main/java/com/chat/handler/IdleDestroyHandler.java
@@ -1,33 +1,50 @@
 package com.chat.handler;
 
+import com.chat.proto.TopLevelDataOuterClass;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 
-public class IdleDestroyHandler extends ChannelInboundHandlerAdapter {
+public class IdleDestroyHandler extends SimpleChannelInboundHandler<TopLevelDataOuterClass.TopLevelData> {
 
-    private int continueIdleTimes = 0;
-    private int maxIdleTimes = 3;
+    private int lossPingTimes = 0;
+    private int maxLossPingTimes = 3;
 
     public IdleDestroyHandler(int maxIdleTimes) {
-        this.maxIdleTimes = maxIdleTimes;
+        this.maxLossPingTimes = maxIdleTimes;
     }
 
     public IdleDestroyHandler() {}
 
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TopLevelDataOuterClass.TopLevelData topLevelData) throws Exception {
+        lossPingTimes = 0;
+        if (topLevelData.getType().equals(TopLevelDataOuterClass.TopLevelData.Type.PING)) {
+            TopLevelDataOuterClass.TopLevelData pingMessage = TopLevelDataOuterClass.TopLevelData.newBuilder()
+                    .setType(TopLevelDataOuterClass.TopLevelData.Type.PONG)
+                    .setSendTime(System.nanoTime())
+                    .build();
+            channelHandlerContext.writeAndFlush(pingMessage);
+        } else {
+            channelHandlerContext.fireChannelRead(topLevelData);
+        }
+    }
+
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println(evt);
         if (evt instanceof IdleStateEvent) {
+            System.out.println("链路空闲通知: " + lossPingTimes);
             IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
             if (idleStateEvent.state() == IdleState.READER_IDLE) {
-                continueIdleTimes++;
-                if (continueIdleTimes < maxIdleTimes) {
+                lossPingTimes++;
+                if (lossPingTimes > maxLossPingTimes) {
+                    System.out.println("因空闲多次，服务端主动关闭链接");
                     ctx.channel().close();
                 }
             }
-        } else {
-            super.userEventTriggered(ctx, evt);
         }
     }
+
+
 }
